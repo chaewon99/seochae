e6274cb89b308116efeed4e93997a71615cd034c
chaewon99 <113489364+chaewon99@users.noreply.github.com>
2022-09-25 16:18:32 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,11 +1,12 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    radio.sendNumber(receivedNumber)
-    basic.showNumber(receivedNumber)
+    radio.sendNumber(순번)
+    basic.showNumber(순번)
 })
 input.onButtonPressed(Button.A, function () {
     if (순번 < 9) {
         순번 += 1
         basic.showNumber(순번)
+        basic.pause(100)
     }
 })
 input.onButtonPressed(Button.AB, function () {
@@ -15,12 +16,14 @@ input.onButtonPressed(Button.B, function () {
     if (순번 > 1) {
         순번 += -1
         basic.showNumber(순번)
+        basic.pause(100)
     }
 })
 let 순번 = 0
-radio.setGroup(1)
+radio.setGroup(100)
 순번 = 1
 basic.showNumber(순번)
+basic.pause(100)
 basic.forever(function () {
     led.stopAnimation()
 })
